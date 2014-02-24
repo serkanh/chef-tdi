c26ffadc31548034b52025abcd6896334b0f42d4
robert <robert@urban4m.com>
2014-02-23 22:01:50 -0500

    remove template repo

--- cookbooks/users/recipes/postgres.rb
+++
@@ -1,7 +0,0 @@
-user "postgres" do
-  shell "/bin/bash"
-  home "/var/lib/postgresql"
-  comment "PostgreSQL"
-  supports :manage_home => true
-  action :create
-end
--- cookbooks/users/recipes/urban4m.rb
+++
@@ -1,7 +0,0 @@
-user "urban4m" do
-  shell "/bin/bash"
-  home "/home/urban4m"
-  comment "Urban4M"
-  supports :manage_home => true
-  action :create
-end
